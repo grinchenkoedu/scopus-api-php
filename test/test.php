0fc785b39c0c5271b2c084635d436bf0063db8ec
Yevhen Matasar <matasar.ei@gmail.com>
2026-09-01 17:11:26 +0300

    test: handle invalid API key exception gracefully

--- test/test.php
+++ test/test.php
@@ -13,31 +13,36 @@ $apiKey = "114ff0c3b57a0ec62e15efdedefd2e6f";
 
 $api = (new ScopusApiFactory($apiKey))->createApiClient();
 
-// Scopus Search API
-$results = $api
-    ->query("af-id(60071066)")
-    ->start(0)
-    ->count(5)
-    ->viewComplete()
-    ->search();
+try {
+    // Scopus Search API
+    $results = $api
+        ->query("af-id(60071066)")
+        ->start(0)
+        ->count(5)
+        ->viewComplete()
+        ->search();
 
-var_dump($results);
+    var_dump($results);
 
-foreach ($results->getEntries() as $entry) {
-    $abstractUrl = $entry->getLinks()->getSelf();
-    
-    // Abstract Retrieval API
-    $abstract = $api->retrieve($abstractUrl);
-    
-    var_dump($abstract);
-
-    $authors = $entry->getAuthors();
-    foreach ($authors as $author) {
-        $authorUrl = $author->getUrl();
+    foreach ($results->getEntries() as $entry) {
+        $abstractUrl = $entry->getLinks()->getSelf();
         
-        // Author Retrieval API
-        $author = $api->retrieve($authorUrl);
+        // Abstract Retrieval API
+        $abstract = $api->retrieve($abstractUrl);
         
-        var_dump($author);
+        var_dump($abstract);
+
+        $authors = $entry->getAuthors();
+        foreach ($authors as $author) {
+            $authorUrl = $author->getUrl();
+            
+            // Author Retrieval API
+            $author = $api->retrieve($authorUrl);
+            
+            var_dump($author);
+        }
     }
+} catch (\GuzzleHttp\Exception\ClientException $e) {
+    echo "API Request failed: " . $e->getMessage() . "\n";
+    echo "Note: If you are seeing a 401 Unauthorized, make sure to replace the dummy API key with a valid one.\n";
 }
