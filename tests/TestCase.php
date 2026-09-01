c0b2a38077a919c80617b95a0c9bffd8ab90b202
Yevhen Matasar <matasar.ei@gmail.com>
2026-09-01 18:16:32 +0300

    test: add base TestCase with Guzzle MockHandler

---
+++ tests/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Scopus\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use Scopus\ScopusApi;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * @param \GuzzleHttp\Promise\PromiseInterface|\Psr\Http\Message\ResponseInterface|\Exception[] $mockResponses
+     * @return ScopusApi
+     */
+    protected function getMockedApi(array $mockResponses): ScopusApi
+    {
+        $mock = new MockHandler($mockResponses);
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+        return new ScopusApi('dummy-key', 30, $client);
+    }
+}
